8658ba44a5425e9de89059c0dbd3bf6ba3504638
Dave Flanagan <dflanagan314@gmail.com>
2016-12-09 22:44:15 -0500

    Initial blast prototype

---
+++ Assets/Scripts/Explosive.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosive : MonoBehaviour {
+
+    [SerializeField]
+    private float effectRadius = 10.0f;
+    [SerializeField]
+    private float blastPressure = 100.0f;
+
+    private bool hasExploded = false;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        debug_event();
+
+    }
+
+    public void debug_event() {
+        if (Input.GetKey(KeyCode.Space)) {
+            detonate();
+            hasExploded = true;
+        }
+    }
+
+    public void detonate() {
+        if (!hasExploded) {
+            Debug.Log("Explode: " + gameObject.name);
+            Collider[] hits = Physics.OverlapSphere(transform.position, effectRadius);
+            for (int i = 0; i < hits.Length; i++) {
+                GameObject go = hits[i].gameObject;
+                ForceReceiver frec = go.GetComponent<ForceReceiver>();
+                if (frec != null) {
+                    Vector3 dirTowardsObj = go.transform.position - this.transform.position;
+                    float dist = dirTowardsObj.magnitude;
+                    dirTowardsObj /= dist;
+                    frec.takeHit(dirTowardsObj, blastPressure, dist);
+                }
+            }
+        } else {
+            Debug.Log("Already exploded: " + gameObject.name);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ForceReceiver.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceReceiver : MonoBehaviour {
+
+    [SerializeField]
+    private int numForceFrames = 4;
+    private int remainingForceFrames;
+
+    private bool forceCalculated = false;
+    private bool used = false;
+    private Vector3 force = Vector3.zero;
+    private Vector3 forcePos = Vector3.zero;
+
+
+    void Start() {
+        remainingForceFrames = numForceFrames;
+    }
+
+    void FixedUpdate() {
+
+        if (!used && forceCalculated) {
+            if (numForceFrames > 0) {
+                applyForce(force, forcePos); //TODO does the force pos need to change per frame?
+                numForceFrames--;
+            } else {
+                forceCalculated = false;
+                used = true;
+            }
+        }
+
+    }
+
+    private void applyForce(Vector3 force, Vector3 forcePos) {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        //Debug.Log("Apply f=" + force + " at p=" + forcePos);
+        rb.AddForceAtPosition(force, forcePos);
+    }
+
+    public void takeHit(Vector3 forceDir, float force, float distFromSrc) {
+        //Debug.Log("Hit taken: force=" + force + " dist=" + distFromSrc);
+        //TODO
+        this.force = forceDir * (force / distFromSrc);
+        this.forcePos = transform.position;
+        forceCalculated = true; //TODO
+    }
+}
