5dbab935ff7848bc5fb7a44e65e9233ff936cde4
Dave Flanagan <dflanagan314@gmail.com>
2016-12-10 09:33:41 -0500

    Implement object grabbing

---
+++ Assets/Scripts/Grabable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class Grabable : VRTK_InteractableObject {
+
+    private int pulsesRemaining = 0;
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();	
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+	}
+
+    public override void Grabbed(GameObject currentGrabbingObject) {
+        base.Grabbed(currentGrabbingObject);
+        Debug.Log("Grabbed " + currentGrabbingObject);
+        //rumble appears to not be implemented yet
+        VRTK_ControllerActions actions = currentGrabbingObject.GetComponent<VRTK_ControllerActions>();
+        if (actions != null) {
+            actions.TriggerHapticPulse((ushort)rumbleOnGrab.x, rumbleOnGrab.y, 0);
+        }
+    }
+
+    public override void StartTouching(GameObject currentTouchingObject) {
+        base.StartTouching(currentTouchingObject);
+        //Debug.Log("Start touching " + currentTouchingObject);
+        VRTK_ControllerActions actions = currentTouchingObject.GetComponent<VRTK_ControllerActions>();
+        if (actions != null) {
+            actions.TriggerHapticPulse((ushort)rumbleOnTouch.x, rumbleOnTouch.y, 0);
+        }
+    }
+}
